3fef7bac11d49e64545a1c40b50516c944e950e7
Andrew Slattery <andrewslattery.13@gmail.com>
2026-01-17 12:32:02 +0000

    moved to cryptics

---
+++ _site/cryptics/import.rb
@@ -0,0 +1,7 @@
+require "jekyll-import"
+
+JekyllImport::Importers::Blogger.run({
+  "source"                => "blog.xml",
+  "no-blogger-info"       => false, 
+  "replace-internal-link" => false
+})
